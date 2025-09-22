eb6e91553107ec32aba5e5c44d263fb001611c6d
JJ <44622919+jj-stuff@users.noreply.github.com>
2025-09-22 11:10:05 -0400

    edit message

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { motion } from 'motion/react';
 import './App.css';
 
 function App() {
-  const MESSAGE = "We've only spent little time together, but I miss you so much. You've become my favorite thought and I'm always thinking of you.\nI hope you can wait for me.";
+  const MESSAGE = "We haven't spent much time together yet, but I miss you more than I expected. This is my embarrassing love letter in return to yours. \nYou've become my favorite thought, I catch myself thinking of you all the time. When I pass a store, I wonder if there's something in there you'd like. When I eat, I would wonder whether you'd love or hate the taste. And with each day I'll only love you more. I cannot wait to see you again. \nI have to admit, I'm quite romantic. jk lol. \nLove you.❤️";
 
   return (
     <>
@@ -11,7 +11,7 @@ function App() {
           <h1>Rak Preme</h1>
         </div>
         <div className="px-64">
-          <p>{MESSAGE}</p>
+          <p className="bg-neutral-700">{MESSAGE}</p>
         </div>
         <div className="flex-row gap-4 flex">
           <motion.button>
